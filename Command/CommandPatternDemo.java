802b14ef32d83bac4ae89b1dde7b394412976f85
eduardoaraujo1 <104658405+eduardoaraujo1@users.noreply.github.com>
2022-05-17 20:48:46 -0300

    Add files via upload

---
+++ Command/CommandPatternDemo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Command;
+
+public class CommandPatternDemo {
+ public static void main(String[] args) {
+ Stock abcStock = new Stock();
+ BuyStock buyStockOrder = new BuyStock(abcStock);
+ SellStock sellStockOrder = new SellStock(abcStock);
+
+ Broker broker = new Broker();
+ broker.takeOrder(buyStockOrder);
+ broker.takeOrder(sellStockOrder);
+ broker.placeOrders();
+ }
+}
